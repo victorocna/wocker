149007cfe7ea2822729dc023d044144647142aa6
victorocna <victorocnarescu@gmail.com>
2019-02-16 17:21:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Wocker
+
+A Wordpress Docker development environment that helps you migrate back and forth between live and local environments.
+
+## Getting started
+
+### Fresh Wordpress install
+
+```bash
+sh local.sh
+```
+
+This will create a Wordpress container with defaults from the `env-example.txt` file.
+
+To edit these options run `cp env-example.txt .env` and modify whatever you wish in the `.env` file.
+
+### Migrate from live versions
+
+First, upload the following files:
+
+* `dump.sql`
+* `wp-content.zip`
+
+After that, refer to the **Migrate SQL** section of `.env` file and update your live and local website URLs.
+
+Optionally, update the Docker images from the **Server config** section of `.env` file based on your live server configuration.
+
+Finally, run the same command as before.
+
+```bash
+sh local.sh
+```
+
+## Debugging
+
+If you encounter errors while bringing up the Docker containers, run the following command. It will destroy **every** docker container and volume. If this doesn't work, just **Reset to factory defaults** from Docker Desktop settings.
+
+```bash
+sh nuke.sh
+```
+
+## License
+
+This project is open-source software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ local.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Remove previous versions
+rm -rf wordpress && mkdir wordpress
+
+# Use env-example if no environment file is provided
+if [ ! -f ./.env ]
+then
+  echo "No environment file provided. Using defaults"
+  source ./env-example.txt
+else
+  source ./.env
+fi
+
+# Create empty dump.sql file if none provided
+if [ ! -f ./dump.sql ]
+then
+  echo "No SQL file provided. Using defaults"
+  touch dump.sql
+else
+  # Migrate SQL: replace live website URL with local URL
+  # Fun tip: to check if this command was successful, count the results like so
+  # grep -o "localhost:8080" wordpress/dump.sql | wc -l
+  sed -e "s|$LIVE_URL|$LOCAL_URL|g" dump.sql > wordpress/dump.sql
+fi
+
+# Unzip the "wp-content" archive if exists
+if [ -f ./wp-content.zip ]
+then
+  echo "Expanding the wp-content archive"
+  unzip -qu wp-content.zip -d wordpress
+fi
+
+# Create Wordpress config
+cd wordpress && touch docker-compose.yml
+cat > docker-compose.yml <<EOL
+version: "2"
+services:
+  db:
+    image: ${DB_IMAGE}
+    restart: always
+    volumes:
+      - mysql:/var/lib/mysql
+      - ./dump.sql:/docker-entrypoint-initdb.d/dump.sql
+    ports:
+      - "3306:3306"
+    environment:
+      MYSQL_DATABASE: ${DB_NAME}
+      MYSQL_USER: ${DB_USER}
+      MYSQL_ROOT_PASSWORD: ${DB_PASS}
+  wordpress:
+    image: ${WP_IMAGE}
+    restart: always
+    volumes:
+      - ./:/var/www/html
+      - ./wp-content/:/var/www/html/wp-content
+    ports:
+      - "8080:80"
+    links:
+      - db:mysql
+    environment:
+      WORDPRESS_DB_HOST: ${DB_HOST}
+      WORDPRESS_DB_NAME: ${DB_NAME}
+      WORDPRESS_DB_USER: ${DB_USER}
+      WORDPRESS_DB_PASSWORD: ${DB_PASS}
+  phpmyadmin:
+    image: phpmyadmin/phpmyadmin
+    environment:
+      MYSQL_ROOT_PASSWORD: ${DB_PASS}
+    ports:
+      - 22222:80
+
+volumes:
+  mysql:
+
+EOL
+
+echo Using $DB_IMAGE as MySQL container
+echo Using $WP_IMAGE as Wordpress container
+
+# Fire up docker containers
+# Visit http://localhost:8080 for sanity check
+echo -e "\nStarting your local development environment. Enjoy!\n"
+docker-compose up -d
